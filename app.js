284c4cb68458388d2fd2bf0950dbefa80d580c37
Uday Kalyan <udaykalyan975@gmail.com>
2024-08-11 22:01:04 +0530

    task2

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const button = document.getElementsByTagName("button");
-console.log(button[0]);
+// Get the button element
+const button = document.getElementById('myButton');
+
+// Get the button's div element
+const divElement = document.getElementsByClassName('button');
+
+// Add a click event listener to the button 
+button.addEventListener('click', () => {
+    // Set a timeout to delay the action
+    setTimeout(() => {
+        //Action to be performed after the delay
+        divElement.innerText = 'Callback executed after 5 seconds';
+        console.log(divElement.innerText);
+    }, 5000); // 5000 milliseconds = 5 seconds
+});
